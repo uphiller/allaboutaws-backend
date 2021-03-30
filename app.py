2998b06f8d9b63a2ea6f9642ee9aa6eeffb17447
곽영호 <1080609@hanwha.com>
2021-03-30 20:56:47 +0900

    수정

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return render_template('index.html')
+    return "Hello, Backend"
 
 @app.route('/fileupload', methods=['POST'])
 def file_upload():
